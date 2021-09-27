b2bc13cbaf820f56df4e3a92739e7ba96b01c724
daveshaul100 <daveshaul100@gmail.com>
2021-09-27 15:28:44 +0100

    change about.js to use styled component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,32 @@
 import React from "react"
 import Layout from "../components/Layout"
 import * as aboutStyles from "../examples/about.module.css"
+import styled from "styled-components"
 
 const about = () => {
   return (
     <Layout>
-      <div className={aboutStyles.page}>
+      <Wrapper>
         <h1>about page</h1>
-        <p className={aboutStyles.text}>
+        <p className="text">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore,
           saepe.
         </p>
-      </div>
+      </Wrapper>
     </Layout>
   )
 }
+
+const Wrapper = styled.section`
+  color: red;
+
+  h1 {
+    color: teal;
+  }
+
+  .text {
+    text-transform: uppercase;
+  }
+`
 
 export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { page, text } from "../examples/home.module.css"
+import { ExampleButton } from "../examples/button"
 
 export default function Home() {
   return (
     <Layout>
       <div className={page}>
+        <ExampleButton>click me</ExampleButton>
         <h1>Home page</h1>
         <p className={text}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, aliquid!
