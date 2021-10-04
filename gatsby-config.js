e79906f7f76dd08b1e82ac0c62d5ea490fb35543
daveshaul100 <daveshaul100@gmail.com>
2021-10-04 14:39:32 +0100

    add webfonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,5 +40,22 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Montserrat",
+              variants: ["400"],
+            },
+            {
+              family: "Inconsolata",
+              variants: ["400", "500", "600", "700"],
+            },
+          ],
+        },
+      },
+    },
   ],
 }
